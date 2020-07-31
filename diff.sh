b1b3398ed6083da03ac11ee2e19848e8ee52dbde
Matthew Tate Scarbrough <matthewtatescarbrough@tutanota.com>
2020-07-30 21:47:22 -0500

    Added self deletion clause

--- diff.sh
+++ diff.sh
@@ -17,16 +17,16 @@
 # ===CHECKS===
 # - - - - - - - - - - - - - - - -
 
-	diff .bash_profile                   ~/.bash_profile                   > "$DIFFS_FILE"/bashpr
-	diff .zshenv                         ~/.zshenv                         > "$DIFFS_FILE"/zshenv
-	diff .config/bash/.bashrc            ~/.config/bash/.bashrc            > "$DIFFS_FILE"/config_bashrc
-	diff .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml > "$DIFFS_FILE"/config_alacritty
-	diff .config/nvim/init.vim           ~/.config/nvim/init.vim           > "$DIFFS_FILE"/config_nvimrc
-	diff .config/sx/sxrc                 ~/.config/sx/sxrc                 > "$DIFFS_FILE"/config_sx
-	diff .config/zsh/.zprofile           ~/.config/zsh/.zprofile           > "$DIFFS_FILE"/config_zpr
-	diff .config/zsh/.zshrc              ~/.config/zsh/.zshrc              > "$DIFFS_FILE"/config_zshrc
-	diff .local/profile/aliases          ~/.local/profile/aliases          > "$DIFFS_FILE"/profile_aliases
-	diff .local/profile/variables        ~/.local/profile/variables        > "$DIFFS_FILE"/profile_variables
+	diff .bash_profile                   ~/.bash_profile                   >  "$DIFFS_FILE"/bashpr
+	diff .zshenv                         ~/.zshenv                         >  "$DIFFS_FILE"/zshenv
+	diff .config/bash/.bashrc            ~/.config/bash/.bashrc            >  "$DIFFS_FILE"/config_bashrc
+	diff .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml >  "$DIFFS_FILE"/config_alacritty
+	diff .config/nvim/init.vim           ~/.config/nvim/init.vim           >  "$DIFFS_FILE"/config_nvimrc
+	diff .config/sx/sxrc                 ~/.config/sx/sxrc                 >  "$DIFFS_FILE"/config_sx
+	diff .config/zsh/.zprofile           ~/.config/zsh/.zprofile           >  "$DIFFS_FILE"/config_zpr
+	diff .config/zsh/.zshrc              ~/.config/zsh/.zshrc              >  "$DIFFS_FILE"/config_zshrc
+	diff .local/profile/aliases          ~/.local/profile/aliases          >  "$DIFFS_FILE"/profile_aliases
+	diff .local/profile/variables        ~/.local/profile/variables        >  "$DIFFS_FILE"/profile_variables
 
 
 # ===REDUNDANCY REMOVAL===
@@ -37,3 +37,5 @@
 		[[ -s "$i" ]] && : || rm -f "$i"
 
 	done
+
+	[[ -z $(ls -A1 "$DIFFS_FILE") ]] && rm -rf "$DIFFS_FILE" || exit
