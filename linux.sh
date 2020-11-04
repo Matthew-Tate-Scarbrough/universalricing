aa7a1134c4a20e87728759f3f1cd003da716eccb
Matþew T. Scarbrough <matthewtatescarbrough@tutanota.com>
2020-11-03 23:30:18 -0600

    Misc

--- linux.sh
+++ linux.sh
@@ -1,58 +1,76 @@
-#!/bin/dash
-# This is not intended for a fresh install, this is just meant to copy files onto a pre-existing install.
-# I've written this because I am tired of copying all changed files over.
+#!/bin/bash
+# This is not intended for a fresh install, this is just meant to copy files on-
+# to a pre-existing install.  I've written this because I am tired of copying
+# all changed files over.
 #
 # TO DO:
 # Write a script to update $PATH/X11/xkb/rules/evdev.xml, then reset xkb data
 
+# Selects either `doas` or `sudo`
+[[ -e /usr/bin/doas && -e /etc/doas.conf ]] && {
+	SUPERU="doas" && alias sudo="doas" ;
+} || SUPERU="sudo"
+
+
 # ===MAKING DIRS===
 
-	mkdir -p $HOME/Downloads/.src				 && \
+	mkdir -p $HOME/Downloads/.src				 &&
 		printf "[1mDownloads done.[0m\n"
-
-	mkdir -p $HOME/Documents/{Assets,C,LaTeX,Go,Rust}	 && \
-	mkdir -p $HOME/Documents/LaTeX/{pdflatex,xelatex}	 && \
-	mkdir -p $HOME/Documents/Assets/LaTeX/Templates		 && \
+	
+	mkdir -p $HOME/Documents/{Assets,C,LaTeX,Go,Rust}	 &&
+	mkdir -p $HOME/Documents/LaTeX/{pdflatex,xelatex}	 &&
+	mkdir -p $HOME/Documents/Assets/LaTeX/Templates		 &&
 		printf "[1mDocuments done.[0m\n"
-
-	mkdir -p $HOME/Pictures/Wallpapers			 && \
+	
+	mkdir -p $HOME/Pictures/Wallpapers			 &&
 		printf "[1mPictures done.[0m\n"
-
-	mkdir -p $HOME/.local/{bin,profile}			 && \
+	
+	mkdir -p $HOME/.local/{bin,profile}			 &&
 		printf "[1m.local done.[0m\n"
-
-	mkdir -p $HOME/.config/{nvim,zsh,bash,afterwriting-labs} && \
+	
+	mkdir -p $HOME/.config/{nvim,zsh,bash,afterwriting-labs} &&
+	mkdir -p $HOME/.config/{vimb,gtk-3.0}                    &&
 		printf "[1m.config done.[0m\n"
 
 	mkdir -p $HOME/.cache/zsh && \
 		printf "[1m.cache done.[0m\n"
 
 	if [ -e /usr/bin/sx ] ; then {
-		printf "[1mI see you're using sx![0m\n" && mkdir -p $HOME/.config/sx && cp -f .config/sx/sxrc $HOME/.config/sx/ && chmod +x $HOME/.config/sx/sxrc
+		printf "[1mI see you're using sx![0m\n"	&&
+		mkdir -p $HOME/.config/sx			&&
+		cp -f .config/sx/sxrc $HOME/.config/sx/		&&
+		chmod +x $HOME/.config/sx/sxrc
 	} else {
-		printf "[1mI see you're not using sx instead of xorg-startx![0m\n" && cp -f .config/sx/sxrc $HOME/.xinitrc
+		printf "[1mI see you're not using sx instead of xorg-startx![0m\n" &&
+		cp -f .config/sx/sxrc $HOME/.xinitrc
 	} fi
 
 
 # ===OVERWRITING FILES===
 
-	# Files directly in ~/
+	# FILES DIRECTLY IN ~/
 	cp -f .bash_profile       $HOME/
 	cp -f .zshenv             $HOME/
 
-	# .config
+
+	# .CONFIG
 	cp -fa .config/.          $HOME/.config/.
 
-	# .local
-	cp -fa .local/bin/.        $HOME/.local/bin/        && chmod -R +x $HOME/.local/bin/*
-	cp -fa .local/profile/.    $HOME/.local/profile/
 
-	# Documents
+	# .LOCAL
+	cp -fa .local/bin/.       $HOME/.local/bin/           &&
+	chmod -R +x $HOME/.local/bin/*
+
+	cp -fa .local/profile/.   $HOME/.local/profile/
+
+
+	# DOCUMENTS
 	cp -f Templates/LaTeX/*   $HOME/Documents/Assets/LaTeX/Templates/
 
-	# Linguist's Dvorak
-	if [ -e /usr/bin/doas ] ; then {
-		printf "[1mI see you're using doas![0m\n" && doas cp -f root/etc/doas.conf /etc/doas.conf || ( sudo cp -f root/etc/doas.conf /etc/doas.conf && doas cp -f keyboards/lingdvorak /usr/share/X11/xkb/symbols/ )
-	} else {
-		printf "[1mI see you're using sudo![0m\n" && sudo cp -f keyboards/lingdvorak /usr/share/X11/xkb/symbols/
-	} fi
+
+	# KEYBOARDS
+	$SUPERU cp -f keyboards/* /usr/share/X11/xkb/symbols/
+
+
+	# DOAS
+	$SUPERU root/etc/doas.conf /etc/
